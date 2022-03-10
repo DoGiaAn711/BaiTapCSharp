40032c0307947f365e5b0d726fddca5569e6b3ef
Dabett <anblcc2002@gmail.com>
2022-03-10 14:30:12 +0700

    first commit

---
+++ learnCSharp/MainProgram.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace learnCSharp {
+    class MainProgram {
+        static void Main(string[] args) {
+            PhanMuc.PM();      
+
+        }
+    }
+}
---
+++ learnCSharp/Phan3/Phan3BaiTap3.cs
@@ -0,0 +1,8 @@
+using System;
+class Phan3BaiTap3 {
+    public static void bt3() {  
+        Console.WriteLine("\"Chào bạn!\"");
+        Console.WriteLine("\"Tôi tên là: Nguyen Van A\"");
+        Console.WriteLine("\"MSSV là: D12CQCN01-N\"");
+    }
+}
