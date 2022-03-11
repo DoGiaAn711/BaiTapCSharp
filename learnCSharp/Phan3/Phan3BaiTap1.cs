333575797d13565f4dd4947313cd4087f849ad12
Dabett <91025829+Dabett@users.noreply.github.com>
2022-03-11 11:03:09 +0700

    Update Phan3BaiTap1.cs

--- learnCSharp/Phan3/Phan3BaiTap1.cs
+++ learnCSharp/Phan3/Phan3BaiTap1.cs
@@ -1,8 +1,17 @@
 using System;
 class Phan3BaiTap1 {
+    struct LUANVAN {
+        public string maLuanVan;
+        public string tenLuanVan; 
+        public string tenSinhVien;
+        public string tenGiangVien;
+        public short namThucHien;
+
+    }
     public static void bt1() {  
-        Console.WriteLine("\"Chào bạn!\"");
-        Console.WriteLine("\"Tôi tên là: Nguyen Van A\"");
-        Console.WriteLine("\"MSSV là: D12CQCN01-N\"");
+        LUANVAN LV;
+        Console.Write("Nhap ma luan van: ");
+        LV.maLuanVan = Console.ReadLine();
+        while (LV.maLuanVan.Length > 10 && LV.maLuanVan.Length != null)
     }
 }
