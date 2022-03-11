55cf6ef8c1f49b4dcc2a7638504a253a8b3ead91
Dabett <91025829+Dabett@users.noreply.github.com>
2022-03-11 11:03:35 +0700

    Update Phan2BaiTap1.cs

--- learnCSharp/Phan2/Phan2BaiTap1.cs
+++ learnCSharp/Phan2/Phan2BaiTap1.cs
@@ -13,7 +13,7 @@ class Phan2BaiTap1 {
         if (a + b > c && a + c > b && b + c > a) {
             Console.WriteLine("Day la 3 canh cua mot tam giac");
             Console.WriteLine("Chu vi tam giac = " + (a + b + c));
-            Console.WriteLine("Dien tich tam giac = " + (Math.Sqrt((a + b + c) * (a + b - c) * (b + c - a) * (c + a - b)) / 4));
+            Console.WriteLine("Dien tich tam giac = {0:0.000}", (Math.Sqrt((a + b + c) * (a + b - c) * (b + c - a) * (c + a - b)) / 4));
         } else Console.WriteLine("Day khong la 3 canh cua mot tam giac");
     }
 }
