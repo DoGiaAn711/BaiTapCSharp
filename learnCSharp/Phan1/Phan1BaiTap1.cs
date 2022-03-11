dd6da057e64e6e63af3c41278421c41205cafc1c
Dabett <91025829+Dabett@users.noreply.github.com>
2022-03-11 11:03:28 +0700

    Update Phan1BaiTap1.cs

--- learnCSharp/Phan1/Phan1BaiTap1.cs
+++ learnCSharp/Phan1/Phan1BaiTap1.cs
@@ -1,7 +1,7 @@
 using System;
 class Phan1BaiTap1 {
     public static void bt1() {  
-        Console.WriteLine("\"Chào bạn!\"");
+        Console.WriteLine("\"Chào ban!\"");
         Console.WriteLine("\"Tôi tên là: Nguyen Van A\"");
         Console.WriteLine("\"MSSV là: D12CQCN01-N\"");
     }
