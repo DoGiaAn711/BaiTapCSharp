a43f10c5a371e7df43a0e1e929f18a379e6ea75e
Nguyễn Việt Anh <96413432+VanhOrigamist@users.noreply.github.com>
2022-03-13 17:23:13 +0700

    Update Phan3BaiTap2.cs

--- learnCSharp/Phan3/Phan3BaiTap2.cs
+++ learnCSharp/Phan3/Phan3BaiTap2.cs
@@ -2,7 +2,7 @@ using System;
 class Phan3BaiTap2 {
     public static void bt2() {  
         Console.WriteLine("\"Chào bạn!\"");
-        Console.WriteLine("\"Tôi tên là: Nguyen Van A\"");
+        Console.WriteLine("\"Tôi tên là: Nguyen Van B\"");
         Console.WriteLine("\"MSSV là: D12CQCN01-N\"");
     }
 }
