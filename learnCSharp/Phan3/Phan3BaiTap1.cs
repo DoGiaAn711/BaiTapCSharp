b2b0c6abec5a87bc6541ac1271a50946c58f3cb3
Dabett <91025829+Dabett@users.noreply.github.com>
2022-03-14 22:22:19 +0700

    update Phan3BaiTap1.cs

--- learnCSharp/Phan3/Phan3BaiTap1.cs
+++ learnCSharp/Phan3/Phan3BaiTap1.cs
@@ -11,7 +11,6 @@ class Phan3BaiTap1 {
 
     }    
     public static void output(LUANVAN LV) {
-        Console.OutputEncoding = Encoding.UTF8; 
         Console.WriteLine("\n-----THÔNG TIN LUẬN VĂN-----");
         Console.WriteLine("Mã luận văn: \t" + LV.maLuanVan);
         Console.WriteLine("Tên luận văn: \t" + LV.tenLuanVan);
@@ -19,23 +18,35 @@ class Phan3BaiTap1 {
         Console.WriteLine("Tên giảng viên: " + LV.tenGiangVien);
         Console.WriteLine("Năm thực hiện: \t" + LV.namThucHien);
     }
+    
     public static void input(LUANVAN LV) {
-        Console.OutputEncoding = Encoding.UTF8; 
         Console.Write("Nhập mã luận văn: ");
         LV.maLuanVan = Console.ReadLine();
-        while (LV.maLuanVan.Length > 11) {
+        while (LV.maLuanVan.Length > 10) {
             Console.Write("Nhập lại mã luận văn: ");
             LV.maLuanVan = Console.ReadLine();
         }
 
         Console.Write("Nhập tên luận văn: ");
         LV.tenLuanVan = Console.ReadLine();
+        while (LV.tenLuanVan.Length > 100) {
+            Console.Write("Nhập lại tên luận văn: ");
+            LV.tenLuanVan = Console.ReadLine();
+        }
 
         Console.Write("Nhập tên sinh viên: ");
         LV.tenSinhVien = Console.ReadLine();
+        while (LV.tenSinhVien.Length > 30) {
+            Console.Write("Nhập lại tên sinh viên: ");
+            LV.tenSinhVien = Console.ReadLine();
+        }
 
         Console.Write("Nhập tên giảng viên: ");
         LV.tenGiangVien = Console.ReadLine();
+        while (LV.tenGiangVien.Length > 30) {
+            Console.Write("Nhập lại tên giảng viên: ");
+            LV.tenGiangVien = Console.ReadLine();
+        }
 
         Console.Write("Nhập năm thực hiện: ");
         LV.namThucHien = short.Parse(Console.ReadLine());
@@ -46,6 +57,7 @@ class Phan3BaiTap1 {
         output(LV);
     }
     public static void bt1() {  
+        Console.OutputEncoding = Encoding.UTF8; 
         LUANVAN LV = new LUANVAN();
         input(LV);
     }
