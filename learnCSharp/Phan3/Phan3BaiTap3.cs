7c5ab8de98c8cb7b8731d4e70b70a6ea091e3ba8
Nguyễn Việt Anh <96413432+VanhOrigamist@users.noreply.github.com>
2022-03-14 20:27:54 +0700

    Update Phan3BaiTap3.cs

--- learnCSharp/Phan3/Phan3BaiTap3.cs
+++ learnCSharp/Phan3/Phan3BaiTap3.cs
@@ -1,8 +1,18 @@
 using System;
-class Phan3BaiTap3 {
-    public static void bt3() {  
-        Console.WriteLine("\"Chào bạn!\"");
-        Console.WriteLine("\"Tôi tên là: Nguyen Van A\"");
-        Console.WriteLine("\"MSSV là: D12CQCN01-N\"");
+namespace CSharpNguVcl {
+    class P3E3 {
+        public static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+            double a=Math.Sqrt(n);
+            double b=Math.Floor(a);
+            if (a==b){
+                Console.WriteLine("true");
+            } else {
+                Console.WriteLine("false");
+            }
+            for (int i = 0; i<=Math.Sqrt(1000);i++){
+                Console.WriteLine(i*i);
+            }
+        }
     }
 }
