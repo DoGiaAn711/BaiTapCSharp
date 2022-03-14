0e43e5a280f4798c019ee68f18046e9d441c1ce4
Dabett <91025829+Dabett@users.noreply.github.com>
2022-03-14 21:30:29 +0700

    update Phan3BaiTap3.cs

--- learnCSharp/Phan3/Phan3BaiTap3.cs
+++ learnCSharp/Phan3/Phan3BaiTap3.cs
@@ -1,8 +1,22 @@
 using System;
+using System.Text;
+
 class Phan3BaiTap3 {
     public static void bt3() {  
-        Console.WriteLine("\"Chào bạn!\"");
-        Console.WriteLine("\"Tôi tên là: Nguyen Van A\"");
-        Console.WriteLine("\"MSSV là: D12CQCN01-N\"");
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.Write("Nhập số cần kiểm tra: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+            double a = Math.Sqrt(n);
+            double b = Math.Floor(a);
+            if (a == b){
+                Console.WriteLine("Đây là số chính phương!");
+            } else {
+                Console.WriteLine("Đây không phải số chính phương!");
+            }
+            
+            Console.WriteLine("Các số chính phương <= 1000: ");
+            for (int i = 0; i <= Math.Sqrt(1000); i++){
+                Console.Write(i * i + " ");
+            }
     }
 }
