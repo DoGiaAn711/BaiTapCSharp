d3cf579595bd454bf3f7688ec9088380d1dd8108
Dabett <91025829+Dabett@users.noreply.github.com>
2022-03-14 18:31:40 +0700

    update Phan2BaiTap4.cs

--- learnCSharp/Phan2/Phan2BaiTap4.cs
+++ learnCSharp/Phan2/Phan2BaiTap4.cs
@@ -1,24 +1,47 @@
 using System;
 using System.Text;
 
-class Phan2BaiTap4 {
-    public static void bt4() {  
-        Console.OutputEncoding = Encoding.UTF8; 
-        Console.Write("Nhập tọa độ điểm A:\nAx = ");
-        double Ax = Convert.ToDouble(Console.ReadLine());
-        Console.Write("Ay = ");
-        double Ay = Convert.ToDouble(Console.ReadLine());
-        
-        Console.Write("Nhập tọa độ điểm B:\nBx = ");
-        double Bx = Convert.ToDouble(Console.ReadLine());
-        Console.Write("By = ");
-        double By = Convert.ToDouble(Console.ReadLine());
-        
-        if ((Ax == Bx && Ay == By)) {
-            Console.Write("2 điểm trùng nhau.");
-        } else {
-            Console.WriteLine("Tọa độ trung điểm C: C({0}; {1})", (Ax + Bx) / 2, (Ay + By) / 2);
-            Console.WriteLine("Độ dài đoạn thẳng AB = {0:0.000}", (Math.Sqrt(Math.Pow(Math.Abs(Ax - Bx), 2) + Math.Pow(Math.Abs(Ay - By), 2))));
+public class Phan2BaiTap4
+{
+    struct Toado
+    {
+        public double x, y;
+        public void Nhap()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập vào x: ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Nhập vào y: ");
+            y = double.Parse(Console.ReadLine());
         }
     }
+    public static void bt4()
+    {
+        Toado A = new Toado();
+        Toado B = new Toado();
+        Console.WriteLine("Tọa độ điểm đầu: ");
+        A.Nhap();
+        Console.WriteLine("Tọa độ điểm cuối: ");
+        B.Nhap();
+        if (A.x == B.x && A.y == B.y)
+        {
+            Console.WriteLine("2 điểm trùng nhau. ");
+        }
+        else
+        {
+            Console.WriteLine("Trung điểm là M(" + Trungdiem(A.x, B.x) + ";" + Trungdiem(A.y, B.y) + ")");
+            Console.WriteLine("Khoảng cách là: " + Khoangcach(A.x, B.x, A.y, B.y));
+        }
+    }
+    public static double Trungdiem(double a, double b)
+    {
+        double trungdiem = (a + b) / 2;
+        return trungdiem;
+    }
+    public static double Khoangcach(double a, double b, double c, double d)
+    {
+        double khoangcach = Math.Sqrt(Math.Pow(a - b, 2) + Math.Pow(c - d, 2));
+        return khoangcach;
+    }
+
 }
