347494af8b3f8ce5753fa71e7cf51696b9e7925a
Dabett <91025829+Dabett@users.noreply.github.com>
2022-03-14 21:02:21 +0700

    update Phan3BaiTap3.cs

--- learnCSharp/Phan3/Phan3BaiTap3.cs
+++ learnCSharp/Phan3/Phan3BaiTap3.cs
@@ -1,4 +1,5 @@
 using System;
+<<<<<<< Updated upstream
 class Phan3BaiTap3 {
     public static void bt3() {  
         Console.WriteLine("\"Chào bạn!\"");
@@ -6,3 +7,21 @@ class Phan3BaiTap3 {
         Console.WriteLine("\"MSSV là: D12CQCN01-N\"");
     }
 }
+=======
+    class Phan3BaiTap3 {
+        public static void bt3() {
+            int n = Convert.ToInt32(Console.ReadLine());
+            double a=Math.Sqrt(n);
+            double b=Math.Floor(a);
+            if (a==b){
+                Console.WriteLine("true");
+            } else {
+                Console.WriteLine("false");
+            }
+            for (int i = 0; i<=Math.Sqrt(1000);i++){
+                Console.WriteLine(i*i);
+            }
+        }
+    }
+
+>>>>>>> Stashed changes
